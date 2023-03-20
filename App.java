648847b69e28ac074f38318792981d6a331ed395
Vijay Omergekar <omergekar.vijay@gmail.com>
2023-03-21 05:04:44 +0530

    Test

---
+++ App.java
@@ -0,0 +1,21 @@
+package com.picsanddabs.demo;
+
+public class App {
+
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(new App().getDetails());
+
+	}
+
+	public String getDetails (){
+		// TODO Auto-generated method stub
+		return "Hello world";
+
+	}
+}
